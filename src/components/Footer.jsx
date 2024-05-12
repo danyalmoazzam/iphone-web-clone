de7b7bade18b01a33b4f7e70cf8490f099772ba0
Danyal Moazzam <danyalmoazzamjan@gmail.com>
2024-05-13 03:35:23 +0500

    👍 fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
               <p key={link} className="font-semibold text-gray text-xs">
                 {link}{' '}
                 {i !== footerLinks.length - 1 && (
-                  <span className="mx-2"> | </span>
+                  <span className="sm:mx-2 mx-2"> | </span>
                 )}
               </p>
             ))}
